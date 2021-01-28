0136d8aea9bb1b3e0f8b828aa622b842d558a363
Marten Moolenaar <marten@zodan.nl>
2021-01-28 22:35:53 +0100

    Added comments

--- inc/scripts.js
+++ inc/scripts.js
@@ -1,8 +1,10 @@
 'use strict';
 
 (function($) {
+	// Give the edit link a title
 	$('#admin-z-mini-menu .btn-edit-post-link').attr('title', 'Edit');
 	
+	// Open and collapse the items
 	$('#expand-z-mini-menu').click(function(e){
 		e.preventDefault();
 		$('#admin-z-mini-menu').addClass('open');
