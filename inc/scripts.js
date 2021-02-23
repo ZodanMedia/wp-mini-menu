64ba91bf333f8536ef48221e9bcf6038bee0f67e
Marten Moolenaar <marten@zodan.nl>
2021-02-24 00:02:09 +0100

    Update v0.0.5
    Jquery ON

--- inc/scripts.js
+++ inc/scripts.js
@@ -1,20 +1,11 @@
-/*
- * WP Mini menu scripts
- *
- */
-
 'use strict';
-
 (function($) {
-	// Give the edit link a title
 	$('#admin-z-mini-menu .btn-edit-post-link').attr('title', 'Edit');
-	
-	// Open and collapse the items
-	$('#expand-z-mini-menu').click(function(e){
+	$( document ).on( "click", "#expand-z-mini-menu", function(e) {
 		e.preventDefault();
 		$('#admin-z-mini-menu').addClass('open');
 	});
-	$('#collapse-z-mini-menu').click(function(e){
+	$( document ).on( "click", "#collapse-z-mini-menu", function(e) {
 		e.preventDefault();
 		$('#admin-z-mini-menu').removeClass('open');
 	});
