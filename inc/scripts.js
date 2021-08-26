50dfa98048dc600a0daa813500433b90c4e08998
Marten Moolenaar <marten@zodan.nl>
2021-08-26 10:30:07 +0200

    Update v0.0.7
    Just cleaned up formatting

--- inc/scripts.js
+++ inc/scripts.js
@@ -7,15 +7,16 @@
  * Author URI: https://zodan.nl
  * License: GPL2
  */
-(function($) {
-	'use strict';
-	$('#admin-z-mini-menu .btn-edit-post-link').attr('title', 'Edit');
-	$( document ).on( "click", "#expand-z-mini-menu", function(e) {
-		e.preventDefault();
-		$('#admin-z-mini-menu').addClass('open');
-	});
-	$( document ).on( "click", "#collapse-z-mini-menu", function(e) {
-		e.preventDefault();
-		$('#admin-z-mini-menu').removeClass('open');
-	});
+
+(function ($) {
+    'use strict';
+    $('#admin-z-mini-menu .btn-edit-post-link').attr('title', 'Edit');
+    $(document).on("click", "#expand-z-mini-menu", function (e) {
+        e.preventDefault();
+        $('#admin-z-mini-menu').addClass('open');
+    });
+    $(document).on("click", "#collapse-z-mini-menu", function (e) {
+        e.preventDefault();
+        $('#admin-z-mini-menu').removeClass('open');
+    });
 })(jQuery);
